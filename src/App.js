af8b35d7ee03e1830684ecdcac4d3a9c0ee1e503
Samim Aktar <samimaktar94320@gmail.com>
2023-08-31 16:50:31 +0530

    All file Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+// import { createClient } from 'pexels';
+// const client = createClient('GvRNfxvaY8yJdqSFPvICFLOrcgtMrKUn7vsR2RgaT8STXk6kUkLKtfPx');
+
+// let query = 'mumbai';
+// client.photos.search({ query }).then(photo => console.log(photo.photos[Math.floor(Math.random() * photo.photos.length)].src.landscape));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <div className="flex">
+
+      </div>
     </div>
   );
 }
